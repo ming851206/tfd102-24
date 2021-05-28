8986732402c02e4042a899285f1714ea4ee2787f
ming851206 <aixian830@gmail.com>
2021-05-28 23:12:10 +0800

    改JS

--- js/common.js
+++ js/common.js
@@ -15,30 +15,23 @@ if (navigator.appVersion.indexOf("Win")!=-1){
   $("bg_block").css('transform','translate(-50%, -50%)');
 };
 
-  // 點擊按鈕，選單和遮罩顯現，
+// 側邊導覽列
 $(function(){
+  // 點擊按鈕，選單和遮罩顯現，
   $("div.menu_bt").on("click", function(){
-    $(this).attr("url", "../img/header_icon/close.svg");
+    // $(this).css("backgroundImage", "url(../img/header_icon/close.svg)");
     $("div.mask").fadeToggle();
     $("div.mobile_navbar").fadeToggle();
-
   });
-});
-
-  // 點擊遮罩，其他東西消失
-$(function(){
-  $("div.mask").on("click", function(){
-    $(this).fadeToggle();
-    $("div.mobile_navbar").fadeToggle();
-
-  });
-});
-
   // 下拉選單縮放
-$(function(){
   $("div.order_flower_mobile").on("click", function(){
     $("ul.flower_list_mobile").slideToggle();
 
   });
-
+  // 點擊遮罩，其他東西消失
+  $("div.mask").on("click", function(){
+    $(this).fadeToggle();
+    $("div.mobile_navbar").fadeToggle();
+    $("div.menu_bt").css("backgroundImage", "url(../img/header_icon/menu.svg)");
+  });
 });
