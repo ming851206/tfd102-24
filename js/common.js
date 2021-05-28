97987cd8a9954021f6d468cd6d52a687a65b85d9
ming851206 <aixian830@gmail.com>
2021-05-28 15:58:53 +0800

    全部加上次選單。 偵測作業系統js

--- js/common.js
+++ js/common.js
@@ -8,9 +8,15 @@
 //   mobile_navbar.add();
 // });
 
+// 偵測作業系統
+if (navigator.appVersion.indexOf("Win")!=-1){
+  $("bg_block").css('transform','translate(-48%, -48%)');
+} else {
+  $("bg_block").css('transform','translate(-50%, -50%)');
+};
 
-$(function(){
   // 點擊按鈕，選單和遮罩顯現，
+$(function(){
   $("div.menu_bt").on("click", function(){
     $(this).attr("url", "../img/header_icon/close.svg");
     $("div.mask").fadeToggle();
@@ -19,8 +25,8 @@ $(function(){
   });
 });
 
-$(function(){
   // 點擊遮罩，其他東西消失
+$(function(){
   $("div.mask").on("click", function(){
     $(this).fadeToggle();
     $("div.mobile_navbar").fadeToggle();
@@ -28,9 +34,8 @@ $(function(){
   });
 });
 
-
-$(function(){
   // 下拉選單縮放
+$(function(){
   $("div.order_flower_mobile").on("click", function(){
     $("ul.flower_list_mobile").slideToggle();
 
