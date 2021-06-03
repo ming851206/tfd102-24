f5ac4f73e8a47ef9207780bce177dc0734eb7baa
ming851206 <aixian830@gmail.com>
2021-06-03 14:35:32 +0800

    刪改圖片

--- js/1_index.js
+++ js/1_index.js
@@ -14,3 +14,19 @@ let preload = src => {
     let img = new Image();
     img.src = src;
 };
+
+// 輪播
+var timer;
+function autopaly(){
+  timer = setInterval(function(){
+    next.onclick()
+  }, 3000);
+}
+autopaly();
+
+var slider = document.getElementById("slider");
+function stopplay(){
+  clearInterval(timer);
+}
+slider.onmouseover = stopplay;
+slider.onmouseout = autopaly;
