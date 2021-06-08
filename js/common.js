886675818c594920598f27a9f41ca3741d5ed362
ming851206 <aixian830@gmail.com>
2021-06-09 03:09:31 +0800

    rwd

--- js/common.js
+++ js/common.js
@@ -9,9 +9,10 @@ if (navigator.appVersion.indexOf("Win")!=-1){
 $(function(){
   // 點擊按鈕，選單和遮罩顯現，
   $("div.menu_bt").on("click", function(){
-    // $(this).css("backgroundImage", "url(../img/header_icon/close.svg)");
     $("div.mask").fadeToggle();
     $("div.mobile_navbar").fadeToggle();
+    $("html").css("position", "fixed");
+    $("html").css("width", "100%");
   });
   // 下拉選單縮放
   $("div.order_flower_mobile").on("click", function(){
@@ -22,7 +23,7 @@ $(function(){
   $("div.mask").on("click", function(){
     $(this).fadeToggle();
     $("div.mobile_navbar").fadeToggle();
-    // $("div.menu_bt").css("backgroundImage", "url(../img/header_icon/menu.svg)");
+    $("html").css("position", "static");
   });
 });
 
@@ -30,9 +31,12 @@ $(function(){
 $(function(){
   $("div.member_bt").on("click", function(){
     $("div.overlay").fadeIn();
+    $("html").css("position", "fixed");
+    $("html").css("width", "100%")
   });
   $("div.close").on("click", function(){
     $("div.overlay").fadeOut();
+    $("html").css("position", "static")
   });
 
   $(function(){
@@ -47,72 +51,3 @@ $(function(){
     });
   });
 });
-
-
-
-// $(function(){
-//   $("div.member_bt").on("click", function(){
-//     var member_page = `
-//     <div class="overlay">
-//       <div class="bg">
-//         <div class="member_list">
-//           <h1 data-target="log_in" class="tab -on">會員登入</h2>
-//           <h1 data-target="sign_in" class="tab">會員註冊</h2>
-//         </div>
-//
-//         <form class="member_content">
-//           <div class="member log_in">
-//             <label>帳號：
-//               <input type="text" placeholder="電子信箱">
-//             </label>
-//
-//             <br>
-//
-//             <label>密碼：
-//               <input type="password" placeholder="英文大小寫有別">
-//             </label>
-//
-//             <br>
-//
-//             <p class="get_pw">忘記密碼</p>
-//
-//             <br>
-//
-//             <button type="submit" class="other log_in">登入</button>
-//           </div>
-//
-//           <div class="member sign_in">
-//             <label>使用者名稱：
-//               <input type="text">
-//             </label>
-//
-//             <br>
-//
-//             <label>電子信箱：
-//               <input type="text">
-//             </label>
-//
-//             <br>
-//
-//             <label>密碼：
-//               <input type="password" placeholder="至少8個字元，包含英文字母及數字">
-//             </label>
-//
-//             <br>
-//
-//             <label>確認密碼：
-//               <input type="password">
-//             </label>
-//
-//             <br>
-//
-//             <button type="submit" class="other">註冊</button>
-//           </div>
-//         </form>
-//       </div>
-//     </div>
-//     `;
-//
-//     $("body").append("member_page");
-//   });
-// });
